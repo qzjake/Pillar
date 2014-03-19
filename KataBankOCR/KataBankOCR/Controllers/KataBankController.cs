c83c4632da142d5594520e3b8b749d5b14ed5211
qzjake <githubGravatarEmail@less-work.com>
2014-03-19 12:40:36 -0500

    Add file upload

--- KataBankOCR/KataBankOCR/Controllers/KataBankController.cs
+++ KataBankOCR/KataBankOCR/Controllers/KataBankController.cs
@@ -1,18 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿#region
+
+using System.Diagnostics;
 using System.Web;
 using System.Web.Mvc;
+
+#endregion
 
 namespace KataBankOCR.Controllers
 {
     public class KataBankController : Controller
     {
-        //
-        // GET: /KataBank/
         public ActionResult Index()
         {
             return View();
         }
-	}
+
+        [HttpPost]
+        public ActionResult Index(HttpPostedFileBase file)
+        {
+            ViewBag.File = file;
+            if (file != null && file.ContentLength > 0)
+            {
+                Debug.WriteLine("This will send the file to our Kata");
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
 }
