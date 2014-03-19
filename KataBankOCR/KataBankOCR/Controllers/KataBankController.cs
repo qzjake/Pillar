a4db96469f457d1f90c937c7b9f79047c5eb3631
qzjake <githubGravatarEmail@less-work.com>
2014-03-19 12:24:24 -0500

    Personalize a little

---
+++ KataBankOCR/KataBankOCR/Controllers/KataBankController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KataBankOCR.Controllers
+{
+    public class KataBankController : Controller
+    {
+        //
+        // GET: /KataBank/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
